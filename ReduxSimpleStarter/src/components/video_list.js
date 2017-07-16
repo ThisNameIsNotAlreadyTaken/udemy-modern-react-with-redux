e26363d29ad410be7d90b6edc5d3afb64202e259
Vasya <eciseeyou@gmail.com>
2017-07-16 22:47:28 +0300

    Section 1 & 2

---
+++ ReduxSimpleStarter/src/components/video_list.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import VideoListItem from './video_list_item';
+
+class VideoList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <ul className="col-md-4 list-group">
+                {this.props.videos.map((video) => <VideoListItem onVideoClick={this.props.onVideoSelect} key={video.etag} video={video}/>)}
+            </ul>
+        );
+    }
+}
+
+export default VideoList;
